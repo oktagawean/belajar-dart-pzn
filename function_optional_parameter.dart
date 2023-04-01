4bf42fe20e900394bb8fca12264436f35e5a3f77
oktagawean <okta.gawean@gmail.com>
2023-04-02 06:08:37 +0700

    menambah middleName pada function_optional_parameter.dart

--- function_optional_parameter.dart
+++ function_optional_parameter.dart
@@ -1,8 +1,9 @@
-void sayHello(String firstName, [String? lastName]) {
-  print('Hello $firstName $lastName');
+void sayHello(String firstName, [String? middleName, String? lastName]) {
+  print('Hello $firstName $middleName $lastName');
 }
 
 void main() {
   sayHello('Okta');
   sayHello('Okta', 'Dhirga');
+  sayHello('Okta', 'Dhirga', 'Sukrisdyanto');
 }
